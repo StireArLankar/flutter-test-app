ad06907a16591011f86a6a0e74d989e4dc6aa299
Stire <stire.ru@gmail.com>
2020-01-05 20:56:18 +0300

    Copypasted app with provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 // import './first_app/first_app.dart';
 // import './second_app/second_app.dart';
-import './third_app/index.dart';
+// import './third_app/index.dart';
+import './fourth_app/fourth_app.dart';
 
 // void main() => runApp(FirstApp());
 // void main() => runApp(SecondApp());
-void main() => runApp(ThirdApp());
+// void main() => runApp(ThirdApp());
+void main() => runApp(FourthApp());
